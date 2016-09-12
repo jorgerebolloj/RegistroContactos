8fb866bc762bda56faccbebe3eb3942b4f87836d
Jorge Rebollo <jorge.rebollo@pademobile.com>
2016-09-11 19:02:58 -0500

    base project

---
+++ RegistroContactos/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RegistroContactos
+//
+//  Created by Jorge Rebollo J on 11/09/16.
+//  Copyright © 2016 RGStudio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
